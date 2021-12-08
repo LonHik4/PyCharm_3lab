3d173fcd0d0213c43ca15e6181bf1fcf780b426d
LONHik4 <gaynullin.dmitriy@gmail.com>
2021-12-09 00:41:48 +0400

    Initial commit

---
+++ 3lab.py
@@ -0,0 +1,54 @@
+import json
+import pathlib
+import pickle
+class ValidPeople:
+    data: list
+
+    def __init__(self, path: pathlib.Path) -> None:
+        self.data = json.load(open(path, encoding='windows-1251'))
+
+
+def quick_sort(alist ,flag:str):
+    #alist список словарей
+    alist = alist[:100]
+    if len(alist) < 2:
+        return
+    borders = [[0, len(alist) - 1]]
+    while len(borders) > 0:
+        left, right = borders.pop()
+        p = alist[(left + right) // 2]
+        i = left - 1
+        j = right + 1
+        while 1:
+            while 1:
+                i += 1
+                if alist[i][flag] >= p[flag]:
+                    break
+            while 1:
+                j -= 1
+                if alist[j][flag] <= p[flag]:
+                    break
+            if i >= j:
+                break
+            alist[i], alist[j] = alist[j], alist[i]
+        if j > left:
+            borders.append([left, j])
+        j += 1
+        if right > j:
+            borders.append([j, right])
+    return alist
+
+def write(path,array):
+    with open(path, 'wb') as write_file:
+        pickle.dump(array, write_file)
+
+def read(path):
+    with open(path, 'rb') as read_file:
+        pickle.load(read_file)
+
+valid_data = ValidPeople(pathlib.Path("correct_data.txt"))
+sort_data = quick_sort(valid_data.data, "height")
+json.dump(sort_data,open("result_sort.txt", "w"),indent=4)
+
+write("serial.pickle", sort_data)
+readfile = read(pathlib.Path('serial.pickle'))
